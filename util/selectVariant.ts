64f431a0a73bc29e8d3fe640060951a1b8a5984e
Chris <cmerrill99@gmail.com>
2024-12-27 12:52:10 -0600

    Revert "logging to debug prod"
    This reverts commit 7558cac34a061dfc974e0832430420b0048951c5.

--- util/selectVariant.ts
+++ util/selectVariant.ts
@@ -2,7 +2,6 @@ export default function<TVariant extends {weight: number}>(variants:TVariant[], 
     const weightTotal = variants.map(v => v.weight).reduce((sum, w) => sum + w);
     let segmentIndex = userId % weightTotal;
     let i = 0;
-    console.log(segmentIndex)
     do {
         var activeVariant = variants[i++];
         segmentIndex -= activeVariant.weight;
