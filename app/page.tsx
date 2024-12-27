b13a1b11b9666b5089aaafbc02d43b231495e717
Chris <cmerrill99@gmail.com>
2024-12-27 11:53:55 -0600

    Cofigure Visual Editing

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,9 @@ import heroExperimentQuery from "@/sanity/queries/heroExperimentQuery";
 import { sanityFetch, SanityLive } from "@/sanity/lib/live";
 import { HeroExperiment } from "@/sanity.types";
 import selectVariant from "@/util/selectVariant";
+import { DisableDraftMode } from "@/components/DisableDraftMode";
+import { VisualEditing } from "next-sanity";
+import { draftMode } from "next/headers";
 
 export default async function Home() {
   const visitorId = Math.ceil(Math.random()*1000000)
@@ -13,6 +16,12 @@ export default async function Home() {
       <h1 className="flex flex-col text-center text-bold text-4xl font-semibold">{heroVariant.titleText}</h1>
       <h4 className="flex flex-col text-center mt-4 text-sm font-thin text-gray-400">{heroVariant.subtitleText}</h4>
       <SanityLive />
+      {(await draftMode()).isEnabled && (
+        <>
+          <DisableDraftMode/>
+          <VisualEditing/>
+        </>
+      )}
     </div>
   );
 }
